ef009934485ee2c9c27f97ee425fc5ef5687e93e
Gabriel <gabrieldantaslp@gmail.com>
2024-09-06 13:44:12 -0300

    feat/api: OrderStatus enum implementation

--- ecommerce-api/src/main/java/com/gabriel/ecommerce_api/entities/Order.java
+++ ecommerce-api/src/main/java/com/gabriel/ecommerce_api/entities/Order.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat;
+import com.gabriel.ecommerce_api.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -21,7 +21,6 @@ import lombok.Setter;
 @Entity
 @Table(name = "tb_order")
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class Order implements Serializable {
@@ -31,14 +30,31 @@ public class Order implements Serializable {
 	@GeneratedValue
 	private UUID id;
 
-  @JsonFormat(
-    shape = JsonFormat.Shape.STRING, 
-    pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", 
-    timezone = "GMT")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+	private Integer orderStatus;
+
+	public Order(UUID id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
+		setOrderStatus(orderStatus);
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
 
 }
